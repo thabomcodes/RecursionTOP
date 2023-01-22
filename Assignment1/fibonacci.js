e9342710ee183d1d6b3269266e9053267a70eb73
Thabo Vincent Moloi <thabomoloicodes@gmail.com>
2023-01-22 15:00:51 +0200

    An fibonacci functions

--- Assignment1/fibonacci.js
+++ Assignment1/fibonacci.js
@@ -1,3 +1,22 @@
-function fibs(num) {
-    
+/**
+ * 
+ * @param {number} n 
+ * @returns 
+ */
+function fibs(n) {
+    const array = [];
+    for (let i = 0; i < n; i++)
+        array.push((i == 0 || i == 1) ? i : array[i - 1] + array[i - 2]);
+    return array;
+}
+
+/**
+ * 
+ * @param {number} n
+ * @returns 
+ */
+function fibsRec(n) {
+    return n < 1 ? [] :
+        (a => a.concat(n === 1 ? [0] : n === 2 ? [1] : [a[a.length - 1] + a[a.length - 2]])
+        )(fibsRec(n - 1));
 }
